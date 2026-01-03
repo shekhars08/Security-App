0825f6586acf84897bf29014834147453a7bdc3c
shekhas08 <shekharsharma0818@gmail.com>
2026-01-04 00:49:07 +0530

    Added UserEntity, UserService

---
+++ src/main/java/com/spring/securityapp/service/UserService.java
@@ -0,0 +1,22 @@
+package com.spring.securityapp.service;
+
+import com.spring.securityapp.exceptions.ResourceNotFoundException;
+import com.spring.securityapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email " + username + " not found"));
+    }
+}
